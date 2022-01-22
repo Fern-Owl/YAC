340da09f4bb95bcdbc755e4e366483ced451c262
Artyom <74039959+Fern-Owl@users.noreply.github.com>
2022-01-22 10:25:26 +0300

    fix: rewrited the calculator for node REPL environment

--- calc.js
+++ calc.js
@@ -1,10 +1,26 @@
-function calcActivator() {
-  // Ввод чисел и выбор операции.
-  let num1 = prompt('Введите первое число:');
-  let operator = prompt('Укажите тип операции (+, -, *, /, %, ^):');
-  let num2 = prompt('Введите второе число:');
-  // Вызов Калькулятора.
-  alert(calc(operator, num1, num2));
+function calcExplainer() {
+  console.log(`
+  Welcome to the Calculator!
+  How to use it:
+  Call the calc() function with three arguments:
+  operator, number 1 and number 2.
+  Like this -> calc('sum', 2, 2)
+  Call calcHelp() to see the list of possible operations.
+    `);
+  return "Let's start calculating!";
+}
+
+function calcHelp() {
+  console.log(`
+  Possible operations include:
+  'sum' = addition
+  'sub' = subtraction
+  'mul' = multiplication
+  'div' = division
+  'mod' = modulus division
+  'pow' = raise to the power
+    `);
+  return "Let's start calculating!";
 }
 
 function calc(operator, num1, num2) {
@@ -12,43 +28,44 @@ function calc(operator, num1, num2) {
   //Проверка на правильность ввода чисел.
   if (isNaN(+num1) || isNaN(+num2)) {
     result = 'Error';
-  } else if (num1 == null || num1.trim() == '') {
+  } else if (num1 == null || num1 == '') {
     result = 'Error';
-  } else if (num2 == null || num2.trim() == '') {
+  } else if (num2 == null || num2 == '') {
     result = 'Error';
   } else {
     switch (operator) {
-      case '+':
+      case 'sum':
         result = `${num1} + ${num2} = ${+num1 + +num2}`;
         break;
-      case '-':
+      case 'sub':
         result = `${num1} - ${num2} = ${num1 - num2}`;
         break;
-      case '*':
+      case 'mul':
         result = `${num1} * ${num2} = ${num1 * num2}`;
         break;
-      case '/':
+      case 'div':
         if (num2 != 0) {
           result = `${num1} / ${num2} = ${num1 / num2}`;
         } else {
           result = `${num1} / ${num2} = ?\nНа ноль делить нельзя!`;
         }
         break;
-      case '%':
+      case 'mod':
         if (num2 != 0) {
           result = `${num1} % ${num2} = ${num1 % num2}`;
         } else {
           result = `${num1} % ${num2} = ?\nНа ноль делить нельзя!`;
         }
         break;
-      case '^':
+      case 'pow':
         result = `${num1} ^ ${num2} = ${num1 ** num2}`;
         break;
       default:
         result = 'unknown operation';
     }
   }
-  return result;
+  console.log(result);
+  return 'Done';
 }
 
-calcActivator();
+calcExplainer();
