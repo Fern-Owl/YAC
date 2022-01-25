0eca7d4c808ec4c9358bcafc83d704025c575d10
Artyom <74039959+Fern-Owl@users.noreply.github.com>
2022-01-25 11:26:49 +0300

    fix: fixed typos, added more tests

--- calc.js
+++ calc.js
@@ -4,18 +4,18 @@ function Calc(operator, num1, num2) {
   const operations = {
     sum: +num1 + +num2,
     sub: num1 - num2,
-    mul: num1 * num2,
+    mult: num1 * num2,
     div: num1 / num2,
   };
 
   if (isNaN(+num1) || isNaN(+num2)) {
-    result = 'Error';
+    result = 'error';
   } else if (num1 === undefined) {
-    result = 'Error';
+    result = 'error';
   } else if (num2 === undefined) {
-    result = 'Error';
+    result = 'error';
   } else if (!(operator in operations)) {
-    result = 'Unknown operation';
+    result = 'unknown operation';
   } else {
     result = operations[operator];
   }
@@ -30,5 +30,8 @@ console.log(Calc('sum', 2, 2));
 console.log(Calc('sub', 10, 3));
 console.log(Calc('div', 144, 12));
 console.log(Calc('div', 1, 0));
-console.log(Calc('mul', 23, 3));
-console.log(Calc('mul', 42, 10));
+console.log(Calc('mult', 23, 3));
+console.log(Calc('mult', 42, 10));
+console.log(Calc('blabla', 0, 0));
+console.log(Calc('sum', 42));
+console.log(Calc('div', 'Mmm?', 13));
